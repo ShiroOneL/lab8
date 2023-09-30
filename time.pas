15b931a0c5b8d1bf97db21a7f95022235f1d8d91
Даниил Авилов <avilovdanya11@gmail.com>
2023-09-30 11:16:31 +0300

    'F

--- time.pas
+++ time.pas
@@ -1,5 +1,5 @@
 ﻿begin
   var year := readinteger('Введите номер года:');
-  if ((year mod 4 = 0) and ((year mod 100 <> 0) and (year mod 400 <> 0))) then print('Год високосный, в нем', 366, 'дней')
+  if (year mod 4 = 0) and ((year mod 100 <> 0) or ((year mod 400 = 0) and (year mod 100 = 0))) then print('Год високосный, в нем', 366, 'дней')
   else print('Год не високосный, в нем', 365, 'дней');
 end.
