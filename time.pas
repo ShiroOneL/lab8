695019dd84074625c5a01dd17b8653c5164ab598
Даниил Авилов <avilovdanya11@gmail.com>
2023-09-30 10:54:33 +0300

    update

---
+++ time.pas
@@ -0,0 +1,5 @@
+﻿begin
+  var year := readinteger('Введите номер года:');
+  if ((year mod 4 = 0) and ((year mod 100 <> 0) and (year mod 400 <> 0))) then print('Год високосный')
+  else print('Год не високосный');
+end.
